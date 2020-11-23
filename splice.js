82e31105109d757cc662393bac9192e78d705f25
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-23 07:47:32 -0500

    -

--- splice.js
+++ splice.js
@@ -1,1 +1,11 @@
-  
+/* The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+const deleted = numbers.splice(2, 3, 6, 7);
+
+console.log(numbers);
+
+console.log(deleted);
