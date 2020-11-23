ef531b045797198eb703d8a26392cb977e3e56e1
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-23 08:04:56 -0500

    -

---
+++ concat.js
@@ -0,0 +1,12 @@
+/* 
+The concat() method concatenates the string arguments to the calling string and returns a new string.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat
+*/
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+const c = a.concat(b);
+
+console.log(c);
