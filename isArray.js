16583d04c2a58112c5ce9aff35ee1e5fb2031d9d
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-24 08:24:19 -0500

    -

---
+++ isArray.js
@@ -0,0 +1,12 @@
+/* 
+The Array.isArray() method determines whether the passed value is an Array.
+If the value is an Array, true is returned; otherwise, false is. 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+*/
+
+const names = ["Daisy", "Maria", "Milagros", "Claudia"];
+const str = "Hello World";
+const number = 27;
+
+console.log(Array.isArray(names));
