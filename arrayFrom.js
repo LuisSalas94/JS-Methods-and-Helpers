12036f13572f0e8799c77ed6ce26e71f7e117663
Luis Salas <luisfernandosalasg@gmail.com>
2020-12-02 06:25:34 -0500

    -

--- arrayFrom.js
+++ arrayFrom.js
@@ -13,13 +13,13 @@ Return value
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 */
 
-const str = "1234567";
+// const str = "1234567";
 
-const result1 = Array.from(str, (number) => {
-	return Number(number);
-});
+// const result1 = Array.from(str, (number) => {
+// 	return Number(number);
+// });
 
-console.log(result1);
+// console.log(result1);
 
 /* 
 The Set constructor lets you create Set objects that store unique values of any type, whether primitive values or object references.
@@ -28,8 +28,28 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 */
 
 //remove the duplicate values
-const numbers = [1, 2, 3, 3, 2, 1, 4, 4, 3, 2, 1, 5];
+// const numbers = [1, 2, 3, 3, 2, 1, 4, 4, 3, 2, 1, 5];
 
-const result = Array.from(new Set(numbers));
+// const result = Array.from(new Set(numbers));
 
-console.log(result);
+// console.log(result);
+
+/* ******************************** */
+
+const text = document.querySelectorAll(".text");
+const newTest = Array.from(text).find((item) => item.innerHTML === "person");
+
+//How to make a dynamic pagination -> Awesome
+const items = Array.from({ length: 120 }, (_, index) => {
+	return index;
+});
+
+const itemsPerPage = 14;
+const pages = Math.ceil(items.length / itemsPerPage);
+const newItems = Array.from({ length: pages }, (_, index) => {
+	const start = index * itemsPerPage;
+	const tempItems = items.slice(start, start + itemsPerPage);
+	return tempItems;
+});
+
+console.log(newItems);
--- reduce.js
+++ reduce.js
@@ -48,16 +48,86 @@ Reduce: iterates, callback function. Reduces to a single value, number, array, o
 
 */
 
-const people = [
-	{ name: "Luis", age: 20, position: "developer", salary: 100 },
-	{ name: "Peter", age: 25, position: "designer", salary: 300 },
-	{ name: "Susy", age: 30, position: "CEO", salary: 400 },
-	{ name: "Anna", age: 35, position: "intern", salary: 10 },
-];
+// const people = [
+// 	{ name: "Luis", age: 20, position: "developer", salary: 100 },
+// 	{ name: "Peter", age: 25, position: "designer", salary: 300 },
+// 	{ name: "Susy", age: 30, position: "CEO", salary: 400 },
+// 	{ name: "Anna", age: 35, position: "intern", salary: 10 },
+// ];
 
-const total = people.reduce((total, person) => {
-	total += person.salary;
-	return total;
-}, 0);
+// const total = people.reduce((total, person) => {
+// 	total += person.salary;
+// 	return total;
+// }, 0);
 
-console.log(total);
+// console.log(total);
+
+//Reduce Objects -> Excelent
+//1st Example
+// const cart = [
+// 	{
+// 		title: "Samsung Galaxi S7",
+// 		price: 599.99,
+// 		amount: 1,
+// 	},
+// 	{
+// 		title: "Google Pixel",
+// 		price: 499.99,
+// 		amount: 2,
+// 	},
+// 	{
+// 		title: "Xiaomi Redmi Note 2",
+// 		price: 699.99,
+// 		amount: 4,
+// 	},
+// 	{
+// 		title: "Xiaomi Redmi Note 5",
+// 		price: 399.99,
+// 		amount: 3,
+// 	},
+// ];
+
+// let total2 = cart.reduce(
+// 	(total, cartItem) => {
+// 		const { amount, price } = cartItem;
+// 		//count items
+// 		total.totalItems += amount;
+
+// 		//count total price
+// 		total.cartTotal += amount * price;
+
+// 		return total;
+// 	},
+// 	{
+// 		totalItems: 0,
+// 		cartTotal: 0,
+// 	}
+// );
+
+// total2.cartTotal = parseFloat(total2.cartTotal.toFixed(2));
+
+// console.log(total2);
+
+//2nd Example -> github repos example
+const url = "https://api.github.com/users/john-smilga/repos?per_page=100";
+
+const fetchRepos = async () => {
+	const response = await fetch(url);
+	const data = await response.json();
+	const newData = data.reduce((total, repo) => {
+		//get data from info
+		const { language } = repo;
+		if (language) {
+			if (total[language]) {
+				total[language] += 1;
+			} else {
+				total[language] = 1;
+			}
+		}
+
+		return total;
+	}, {});
+	console.log(newData);
+};
+
+fetchRepos();
